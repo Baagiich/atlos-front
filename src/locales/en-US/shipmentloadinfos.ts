7134ffb38ae2cbda6286080a064625583c1a8ab2
khandsuren <khandsuren@lodipay.mn>
2024-01-15 12:34:45 +0800

    fix: translation

---
+++ src/locales/en-US/shipmentloadinfos.ts
@@ -0,0 +1,8 @@
+export default {
+    name: "Name",
+    packageType: "Package type",
+    count: "Count",
+    weight: "Weight",
+    cube: "Cube",
+  };
+  
---
+++ src/locales/mn-MN/shipmentloadinfos.ts
@@ -0,0 +1,8 @@
+export default {
+    name: "Нэр",
+    packageType: "Ачааны төрөл",
+    count: "Тоо ширхэг",
+    weight: "Жин /кг",
+    cube: "Эзэлхүүн",
+  };
+  
