58142942ce1223e7a3fe61131fd23f3a61126f28
khandsuren <khandsuren@lodipay.mn>
2024-01-15 12:24:04 +0800

    fix: translation

--- src/locales/zh-Hans/shippercompany.ts
+++ src/locales/zh-Hans/shippercompany.ts
@@ -1,9 +1,9 @@
 export default {
   firstname: "名",
   lastname: "性",
-  name: "运输名称",
+  name: "运输公司名称",
   email: "电子邮件",
-  register: "运输等级",
+  register: "运输公司登记",
   phoneNumber: "电话号码",
   title: "运输公司",
 };
