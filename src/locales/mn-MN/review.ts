a2261ea631da7033c6ffc9053c55dbcc23b7b90d
khandsuren <khandsuren@lodipay.mn>
2024-01-21 13:35:58 +0800

    fix: review tag translated

--- src/locales/mn-MN/review.ts
+++ src/locales/mn-MN/review.ts
@@ -1,4 +1,10 @@
 export default {
   confirmTitle: "Тухайн тээвэрлэлтийг үнэлнэ үү",
   confirmBtn: "Батлах",
+  quick: "Хурдан",
+  trustworthy: "Итгэлтэй",
+  secure: "Найдвартай",
+  lackOfInformation: "Мэдээлэл дутуу өгсөн",
+  notPaid: "Төлбөр төлөөгүй",
+  combination: "Бүгд",
 };
---
+++ src/types/reviewTagtype.ts
@@ -0,0 +1,9 @@
+export enum ReviewTagType {
+  // өөрчилж үл болно. Back дээр байгаа үгнүүд шү
+  quick = "Хурдан",
+  trustworthy = "Итгэлтэй",
+  secure = "Найдвартай",
+  lackOfInformation = "Мэдээлэл дутуу өгсөн",
+  notPaid = "Төлбөр төлөөгүй",
+  combination = "Combination",
+}
